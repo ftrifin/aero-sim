f588adeddcf2a477af1e768350193daecb7100dd
ryan.osborn117 <ryan.osborn117@gmail.com>
2012-02-16 17:26:52 +0000

    Added translational equations of motion model to Dynamics category

---
+++ Models/Dynamics/TranslationalEoM/Dynamics_TranslationalEoM_Outputs_BusObject.m
@@ -0,0 +1,46 @@
+function Dynamics_TranslationalEoM_Outputs_BusObject() 
+% DYNAMICS_TRANSLATIONALEOM_OUTPUTS_BUSOBJECT initializes a set of bus objects in the MATLAB base workspace 
+
+% Bus object: Dynamics_TranslationalEoM_Outputs 
+clear elems;
+elems(1) = Simulink.BusElement;
+elems(1).Name = 'Acceleration_vec_FI';
+elems(1).Dimensions = 3;
+elems(1).DimensionsMode = 'Fixed';
+elems(1).DataType = 'double';
+elems(1).SampleTime = -1;
+elems(1).Complexity = 'real';
+elems(1).SamplingMode = 'Sample based';
+elems(1).Min = [];
+elems(1).Max = [];
+
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'Velocity_vec_FI';
+elems(2).Dimensions = 3;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'double';
+elems(2).SampleTime = -1;
+elems(2).Complexity = 'real';
+elems(2).SamplingMode = 'Sample based';
+elems(2).Min = [];
+elems(2).Max = [];
+
+elems(3) = Simulink.BusElement;
+elems(3).Name = 'Position_vec_FI';
+elems(3).Dimensions = 3;
+elems(3).DimensionsMode = 'Fixed';
+elems(3).DataType = 'double';
+elems(3).SampleTime = -1;
+elems(3).Complexity = 'real';
+elems(3).SamplingMode = 'Sample based';
+elems(3).Min = [];
+elems(3).Max = [];
+
+Dynamics_TranslationalEoM_Outputs = Simulink.Bus;
+Dynamics_TranslationalEoM_Outputs.HeaderFile = '';
+Dynamics_TranslationalEoM_Outputs.Description = sprintf('');
+Dynamics_TranslationalEoM_Outputs.DataScope = 'Auto';
+Dynamics_TranslationalEoM_Outputs.Alignment = -1;
+Dynamics_TranslationalEoM_Outputs.Elements = elems;
+assignin('base', 'Dynamics_TranslationalEoM_Outputs', Dynamics_TranslationalEoM_Outputs)
+
