cd3c9bc75ed3fbcce82f58dee9fac5006cc8c407
ryan.osborn117@gmail.com <ryan.osborn117@gmail.com@585f20bd-c0bf-0087-86a7-397fe84cf71d>
2012-02-18 10:12:32 +0000

    Moved quaternion multiplication test harness in to UnitTest subfolder, added test script, added qmult script to utilities folder and added TestResults subfolder

---
+++ trunk/Models/Mathematics/QuaternionMultiplication/UnitTest/Mathematics_QuaternionMultiplication_Test.m
@@ -0,0 +1,59 @@
+%% Mathematics_QuaternionMultiplication Model Test %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc
+
+diary_file = [getenv('AeroSim_Install') '\Models\Mathematics\QuaternionMultiplication\TestResults\Mathematics_QuaternionMultiplication_Test_' date '.txt'];
+if exist(diary_file, 'file')
+    delete(diary_file)
+end
+diary(diary_file)
+
+disp('-------------------------------------------------------------------')
+disp('Mathematics_QuaternionMultiplication Model Test')
+disp('-------------------------------------------------------------------')
+
+%% Initialisation
+tolerance = 0.01;
+time_span = 0;
+
+%% Inputs
+input_q1 = [0.7071; 0.7071; 0; 0];
+input_q2 = [0.7071; 0; 0.7071; 0];
+
+%% Expected Output
+expected_output_q = qmult(input_q1, input_q2);
+
+%% Simulation
+sim('Mathematics_QuaternionMultiplication_TH', time_span)
+
+%% Error Calculation
+error = (output_q' - expected_output_q) ./ expected_output_q;
+
+%% Test Result
+disp('Input:')
+disp(['    v1 = [' num2str(input_q1') ']'])
+disp(['    v2 = [' num2str(input_q2') ']'])
+disp(' ')
+disp('Expected Output:')
+disp(['    v = [' num2str(expected_output_q') ']'])
+disp(' ')
+disp('Actual Output:')
+disp(['    v = [' num2str(output_q) ']'])
+disp(' ')
+disp('Error:')
+disp(['    e = [' num2str(error') ']'])
+disp(' ')
+disp('Tolerance:')
+disp(['    t = ' num2str(tolerance)])
+
+disp('-------------------------------------------------------------------')
+if all(error) <= tolerance
+    disp('TEST PASSED')
+else
+    disp('TEST FAILED')
+end
+disp('-------------------------------------------------------------------')
+
+%% Clean Up
+diary off
+clear diary_file time_span tolerance input_q1 input_q2 expected_output_q output_q error
