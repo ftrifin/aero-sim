fbd0715b04024237eba7c01136f506355ffe2466
ryan.osborn117@gmail.com <ryan.osborn117@gmail.com@585f20bd-c0bf-0087-86a7-397fe84cf71d>
2012-02-18 10:43:23 +0000

    Moved quaternion derivative test harness in to UnitTest subfolder, added test script, added qderiv script to utilities folder and added TestResults subfolder

---
+++ trunk/Models/Mathematics/QuaternionDerivative/UnitTest/Mathematics_QuaternionDerivative_Test.m
@@ -0,0 +1,59 @@
+%% Mathematics_QuaternionDerivative Model Test %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc
+
+diary_file = [getenv('AeroSim_Install') '\Models\Mathematics\QuaternionDerivative\TestResults\Mathematics_QuaternionDerivative_Test_' date '.txt'];
+if exist(diary_file, 'file')
+    delete(diary_file)
+end
+diary(diary_file)
+
+disp('-------------------------------------------------------------------')
+disp('Mathematics_QuaternionDerivative Model Test')
+disp('-------------------------------------------------------------------')
+
+%% Initialisation
+tolerance = 0.01;
+time_span = 0;
+
+%% Inputs
+input_q = [0.5; 0.5; 0.5; 0.5];
+input_w = [1; 2; 4]*pi/180;
+
+%% Expected Output
+expected_output_dq = qderiv(input_q, input_w);
+
+%% Simulation
+sim('Mathematics_QuaternionDerivative_TH', time_span)
+
+%% Error Calculation
+error = (output_dq' - expected_output_dq) ./ expected_output_dq;
+
+%% Test Result
+disp('Input:')
+disp(['    q = [' num2str(input_q') ']'])
+disp(['    w = [' num2str(input_w') ']'])
+disp(' ')
+disp('Expected Output:')
+disp(['    dq = [' num2str(expected_output_dq') ']'])
+disp(' ')
+disp('Actual Output:')
+disp(['    dq = [' num2str(output_dq) ']'])
+disp(' ')
+disp('Error:')
+disp(['    e = [' num2str(error') ']'])
+disp(' ')
+disp('Tolerance:')
+disp(['    t = ' num2str(tolerance)])
+
+disp('-------------------------------------------------------------------')
+if all(error) <= tolerance
+    disp('TEST PASSED')
+else
+    disp('TEST FAILED')
+end
+disp('-------------------------------------------------------------------')
+
+%% Clean Up
+diary off
+clear diary_file time_span tolerance input_q input_w expected_output_dq output_dq error
