59dd812ae2cb78c217fc7add6ae7091183952df8
ryan.osborn117@gmail.com <ryan.osborn117@gmail.com@585f20bd-c0bf-0087-86a7-397fe84cf71d>
2012-03-02 16:16:56 +0000

    Added slblocks.m file to allow AML to be shown in Simulink Library Browser

---
+++ trunk/slblocks.m
@@ -0,0 +1,6 @@
+function blkStruct = slblocks
+  % Specify that the product should appear in the library browser
+  % and be cached in its repository
+  Browser.Library = 'AeroSim';
+  Browser.Name    = 'AeroSim Library';
+  blkStruct.Browser = Browser;
